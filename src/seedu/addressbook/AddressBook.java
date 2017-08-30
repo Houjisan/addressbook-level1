1692b8ae3183a723d97eda6cf62f57c91dfa90cf
Houjisan <hongjing10188@gmail.com>
2017-08-31 01:50:48 +0900

    Changed find command to include phone number and email address as search keywords.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -451,7 +451,7 @@ public class AddressBook {
      */
     private static String executeFindPersons(String commandArgs) {
         final Set<String> keywords = extractKeywordsFromFindPersonArgs(commandArgs);
-        final ArrayList<String[]> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        final ArrayList<String[]> personsFound = getPersonsWithAnyKeyword(keywords);
         showToUser(personsFound);
         return getMessageForPersonsDisplayedSummary(personsFound);
     }
@@ -482,11 +482,13 @@ public class AddressBook {
      * @param keywords for searching
      * @return list of persons in full model with name containing some of the keywords
      */
-    private static ArrayList<String[]> getPersonsWithNameContainingAnyKeyword(Collection<String> keywords) {
+    private static ArrayList<String[]> getPersonsWithAnyKeyword(Collection<String> keywords) {
         final ArrayList<String[]> matchedPersons = new ArrayList<>();
         for (String[] person : getAllPersonsInAddressBook()) {
-            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final Set<String> personalDetails = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
+            personalDetails.add(getPhoneFromPerson(person));
+            personalDetails.add(getEmailFromPerson(person));
+            if (!Collections.disjoint(personalDetails, keywords)) {
                 matchedPersons.add(person);
             }
         }
